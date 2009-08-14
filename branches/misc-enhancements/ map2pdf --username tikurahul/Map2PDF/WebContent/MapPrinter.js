663643ec464337fd02df62d3a435ad79138f8bd9
cpuffalt <cpuffalt@a12b1006-6721-11de-a8c1-9da277d31688>
2009-08-14 15:45:56 +0000

    Fix so that the map.graphics layer gets rendered in addition to the map.graphicslayerIds layers.

--- branches/misc-enhancements/ map2pdf --username tikurahul/Map2PDF/WebContent/MapPrinter.js
+++ branches/misc-enhancements/ map2pdf --username tikurahul/Map2PDF/WebContent/MapPrinter.js
@@ -1,4 +1,4 @@
-﻿﻿dojo.provide("MapPrinter");
+﻿dojo.provide("MapPrinter");
 dojo.require("dojo.io.iframe");
 
 dojo.declare("MapPrinter", "Object", {
@@ -24,59 +24,108 @@ dojo.declare("MapPrinter", "Object", {
     return url;
   },
   
+  _renderLayer: function(layer, tileUrls, dynamicUrls, features) {
+    var extent = this.map.extent;
+    var height = this.map.height;
+    var width = this.map.width;
+
+    if (layer instanceof esri.layers.ArcGISTiledMapServiceLayer) {
+      var candidateTileInfo = esri.TileUtils.getCandidateTileInfo(this.map,
+          layer.tileInfo, extent);
+      var layerTileUrls = [];
+      var tileXOffset = Math.ceil(width / layer.tileInfo.width);
+      var tileYOffset = Math.ceil(height / layer.tileInfo.height);
+      for ( var x = 0; x <= tileXOffset; x++) {
+        for ( var y = 0; y <= tileYOffset; y++) {
+          var tileUrl = layer.url + "/tile/" + this.map.getLevel() + "/"
+              + (candidateTileInfo.tile.coords.row + y) + "/"
+              + (candidateTileInfo.tile.coords.col + x);
+          layerTileUrls.push( {
+            "url" : this._convertUrlToAbsolute(tileUrl),
+            "row" : candidateTileInfo.tile.coords.row + y,
+            "col" : candidateTileInfo.tile.coords.col + x
+          });
+        }
+      }
+      tileUrls.push( {
+        "tiles" : layerTileUrls,
+        "transparency" : layer.opacity,
+        "clipOptions" : {
+          "offsetX" : candidateTileInfo.tile.offsets.x
+              - this.map._visibleDelta.x,
+          "offsetY" : candidateTileInfo.tile.offsets.y
+              - this.map._visibleDelta.y,
+          "width" : width,
+          "height" : height
+        }
+      });
+    } else if (layer instanceof esri.layers.ArcGISDynamicMapServiceLayer) {
+      var dynamicUrl = layer.url + "/export?bbox=" + extent.xmin + ","
+          + extent.ymin + "," + extent.xmax + "," + extent.ymax
+          + "&size=" + width + "," + height
+          + "&transparent=true&format=png24&f=image";
+      dynamicUrls.push( {
+        "url" : this._convertUrlToAbsolute(dynamicUrl),
+        "width" : this.map.width,
+        "height" : this.map.height,
+        "transparency" : layer.opacity
+      });
+    } else if (layer instanceof esri.layers.GraphicsLayer) {
+      var geometries = [];
+      var colors = [];
+      
+      var graphics = layer.graphics;
+      for ( var i = 0; i < graphics.length; i++) {
+        var geometry = esri.geometry.toScreenGeometry(
+          extent, width, height, graphics[i].geometry);
+        if (geometry instanceof esri.geometry.Polygon) {
+          geometries.push({
+            "geometryType" : "esriGeometryPolygon",
+            "rings" : geometry.rings,
+            "symbol" : graphics[i].symbol.toJson()
+          });
+        }
+        else if (geometry instanceof esri.geometry.Point) {
+          geometries.push({
+            "geometryType" : "esriGeometryPoint",
+            "x" : geometry.x,
+            "y" : geometry.y,
+            "symbol" : graphics[i].symbol.toJson()
+          });
+        }
+        else if (geometry instanceof esri.geometry.Polyline) {
+          geometries.push({
+            "geometryType" : "esriGeometryPolyline",
+            "paths" : geometry.paths,
+            "symbol" : graphics[i].symbol.toJson()
+          });
+        }
+        else {
+          console.log("Unknown geometry: ", geometry);
+        }
+        
+        colors.push( graphics[i].symbol.color);
+      }
+      
+      features.push({
+        "geometries" : geometries,
+        "colors" : colors,
+        "transparency" : 0.5,
+        "width" : width,
+        "height" : height
+      });
+    }
+
+  },
+  
   layersJson : function() {
     var tileUrls = [];
     var dynamicUrls = [];
     
-    var extent = this.map.extent;
-    var height = this.map.height;
-    var width = this.map.width;
     
     dojo.forEach(this.map.layerIds, dojo.hitch(this, function(layerId) {
       var layer = this.map.getLayer(layerId);
-      
-      if (layer instanceof esri.layers.ArcGISTiledMapServiceLayer) {
-        var candidateTileInfo = esri.TileUtils.getCandidateTileInfo(this.map,
-            layer.tileInfo, extent);
-        var layerTileUrls = [];
-        var tileXOffset = Math.ceil(width / layer.tileInfo.width);
-        var tileYOffset = Math.ceil(height / layer.tileInfo.height);
-        for ( var x = 0; x <= tileXOffset; x++) {
-          for ( var y = 0; y <= tileYOffset; y++) {
-            var tileUrl = layer.url + "/tile/" + this.map.getLevel() + "/"
-                + (candidateTileInfo.tile.coords.row + y) + "/"
-                + (candidateTileInfo.tile.coords.col + x);
-            layerTileUrls.push( {
-              "url" : this._convertUrlToAbsolute(tileUrl),
-              "row" : candidateTileInfo.tile.coords.row + y,
-              "col" : candidateTileInfo.tile.coords.col + x
-            });
-          }
-        }
-        tileUrls.push( {
-          "tiles" : layerTileUrls,
-          "transparency" : layer.opacity,
-          "clipOptions" : {
-            "offsetX" : candidateTileInfo.tile.offsets.x
-                - this.map._visibleDelta.x,
-            "offsetY" : candidateTileInfo.tile.offsets.y
-                - this.map._visibleDelta.y,
-            "width" : width,
-            "height" : height
-          }
-        });
-      } else if (layer instanceof esri.layers.ArcGISDynamicMapServiceLayer) {
-        var dynamicUrl = layer.url + "/export?bbox=" + extent.xmin + ","
-            + extent.ymin + "," + extent.xmax + "," + extent.ymax
-            + "&size=" + width + "," + height
-            + "&transparent=true&format=png24&f=image";
-        dynamicUrls.push( {
-          "url" : this._convertUrlToAbsolute(dynamicUrl),
-          "width" : this.map.width,
-          "height" : this.map.height,
-          "transparency" : layer.opacity
-        });
-      }
+      this._renderLayer(layer, tileUrls, dynamicUrls, null);
     }));
     
     return dojo.toJson({
@@ -88,60 +137,15 @@ dojo.declare("MapPrinter", "Object", {
   featuresJson : function() {
     var features = [];
     
-    var extent = this.map.extent;
-    var height = this.map.height;
-    var width = this.map.width;
-
     dojo.forEach(this.map.graphicsLayerIds, dojo.hitch(this, function(layerId) {
       var layer = this.map.getLayer(layerId);
-      
-      if (layer instanceof esri.layers.GraphicsLayer) {
-        var geometries = [];
-        var colors = [];
-        
-        var graphics = layer.graphics;
-        for ( var i = 0; i < graphics.length; i++) {
-          var geometry = esri.geometry.toScreenGeometry(
-            extent, width, height, graphics[i].geometry);
-          if (geometry instanceof esri.geometry.Polygon) {
-            geometries.push({
-              "geometryType" : "esriGeometryPolygon",
-              "rings" : geometry.rings,
-              "symbol" : graphics[i].symbol.toJson()
-            });
-          }
-          else if (geometry instanceof esri.geometry.Point) {
-            geometries.push({
-              "geometryType" : "esriGeometryPoint",
-              "x" : geometry.x,
-              "y" : geometry.y,
-              "symbol" : graphics[i].symbol.toJson()
-            });
-          }
-          else if (geometry instanceof esri.geometry.Polyline) {
-            geometries.push({
-              "geometryType" : "esriGeometryPolyline",
-              "paths" : geometry.paths,
-              "symbol" : graphics[i].symbol.toJson()
-            });
-          }
-          else {
-            console.log("Unknown geometry: ", geometry);
-          }
-          
-          colors.push( graphics[i].symbol.color);
-        }
-        
-        features.push({
-          "geometries" : geometries,
-          "colors" : colors,
-          "transparency" : 0.5,
-          "width" : width,
-          "height" : height
-        });
-      }
+      this._renderLayer(layer, null, null, features);
     }));
-
+    
+    if (this.map.graphics != null) {
+      this._renderLayer(this.map.graphics, null, null, features);
+    }
+    
     return dojo.toJson(features);
   },
   
